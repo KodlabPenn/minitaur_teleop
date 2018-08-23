0d2cd1ccad834dfca10b82edf4a5b2b0a34baa39
Vasileios Vasilopoulos <vasilis.vasilop@gmail.com>
2018-08-23 14:16:16 -0400

    Changed script to support Ghost SDK

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# minitaur_teleop
+# Minitaur remote control with ROS
+
+This package is based on the [Ghost Robotics SDK](http://ghostrobotics.gitlab.io/SDK/). See the `CommandOverEthernet` example in the SDK to properly setup the ROS bridge over Ethernet.
+
+## Usage
+Once the ROS bridge has been setup and running, launch `minitaur_teleop.launch` to control the robot. Here we assume that an Xbox-360 controller is used.
--- src/command_publisher.py
+++ src/command_publisher.py
@@ -1,62 +1,54 @@
 #!/usr/bin/env python
 import rospy, struct
 from sensor_msgs.msg import Joy
-from std_msgs.msg import UInt8, Float32
+from std_msgs.msg import UInt32, Float32
 from geometry_msgs.msg import Twist
 
 # Define publishers
-pub1 = rospy.Publisher('/set_cmd', UInt8, queue_size=100)
-pub2 = rospy.Publisher('/set_twist', Twist , queue_size = 100)
+pub_behaviorId = rospy.Publisher('/set_behaviorId', UInt32, queue_size=1)
+pub_behaviorMode = rospy.Publisher('/set_behaviorMode', UInt32, queue_size=1)
+pub_twist = rospy.Publisher('/set_twist', Twist , queue_size=1)
 
-# Initialize state
-state = 0
+# Initialize global behavior and mode variables
+behavior = 0
+mode = 0
 
 # Available commands
-CMD_KILL = 0
-CMD_STAND = 1
-CMD_START = 2
-CMD_SET_POSITION = 3
-CMD_SET_OPEN_LOOP = 4
-CMD_SET_GAIT_BOUND = 5
-CMD_SET_GAIT_WALK = 6
-CMD_SET_GAIT_PUSH = 7
-CMD_SET_GAIT_JUMPON = 8
-CMD_SIGNAL0 = 10
-CMD_SIGNAL1 = 11
-CMD_SIGNAL2 = 12
-CMD_SIGNAL3 = 13
+BEHAVIOR_BOUND = 0
+BEHAVIOR_WALK = 1
+MODE_STAND = 0
+MODE_START = 1
 
 def callback(data):
-	global state 
-	r = rospy.Rate(1000)
-	
-	#X
+	global behavior, mode
+
+	#X - unused
 	if (data.buttons[2] == 1):
-		state = CMD_KILL
+		a = 1
 	#A
 	elif (data.buttons[0] == 1):
-		state = CMD_STAND
+		mode = MODE_STAND
 	#START
 	elif (data.buttons[7] == 1):
-		state = CMD_START
+		mode = MODE_START
 	#B
 	elif (data.buttons[1] == 1):
-		state = CMD_SET_GAIT_BOUND
+		behavior = BEHAVIOR_BOUND
 	#Y
 	elif (data.buttons[3] == 1):
-		state = CMD_SET_GAIT_WALK
-	#RB
+		behavior = BEHAVIOR_WALK
+	#RB - unused
 	elif (data.buttons[5] == 1):
-		state = CMD_SET_GAIT_JUMPON
+		a = 1
 	#LB
 	elif (data.buttons[4] == 1):
-		state = CMD_SET_GAIT_PUSH
+		a = 1
 	
 	# Define twist
 	twist = Twist()
 
 	# Define forward velocity
-	lin_x = data.axes[4] * 0.5 
+	lin_x = data.axes[4] * 0.5
 	twist.linear.x = lin_x
 
 	# Define angular yaw rate
@@ -69,9 +61,11 @@ def callback(data):
 	twist.linear.z = height
 	
 	# Log info and publish
-	rospy.loginfo("%d %.4f %.4f %.4f", state, lin_x, angular_z, height)
-	pub1.publish(state)
-	pub2.publish(twist)
+	rospy.loginfo("%d %d %.4f %.4f %.4f", behavior, mode, lin_x, angular_z, height)
+	pub_behaviorId.publish(behavior)
+	pub_behaviorMode.publish(mode)
+	pub_twist.publish(twist)
+	return
 
 def init():
 	
